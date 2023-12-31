831752e62c7ee85ce5a8eecf4d0b468fb85d5155
Joel Vandenberg <jtvberg@gmail.com>
2023-12-31 14:49:20 -0600

    packate update

--- build/afterSign.js
+++ build/afterSign.js
@@ -1,16 +1,17 @@
-const { notarize } = require('electron-notarize')
+const { notarize } = require('@electron/notarize')
 
-exports.default = function (context) {
+exports.default = async function (context) {
   console.log('Notarizing')
   // Get contex vars
   const appName = context.packager.appInfo.productFilename
   const appDir = context.appOutDir
 
   // Notarize
-  return notarize({
+  return await notarize({
     appBundleId: 'com.jtvberg.tidypaste',
     appPath: `${appDir}/${appName}.app`,
     appleId: process.env.appleId,
-    appleIdPassword: process.env.appleIdPassword
+    appleIdPassword: process.env.appleIdPassword,
+    teamId: process.env.teamId
   })
 }
